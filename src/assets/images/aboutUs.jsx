9db0b870dc87f6341d9fe0c742e950d710c8cb99
Sito8943 <sito8943@gmail.com>
2022-11-27 22:32:02 -0500

    doing about

--- src/assets/images/aboutUs.jsx
+++ src/assets/images/aboutUs.jsx
@@ -1,7 +1,9 @@
 import mission from "./mission.png";
 import values from "./values.png";
 import vision from "./vision.png";
+import goal from "./goal.png";
+import strategy from "./strategy.png";
 
-const aboutUs = { mission, values, vision };
+const aboutUs = { mission, values, vision, goal, strategy };
 
 export default aboutUs;
--- src/views/Home/AboutUs/AboutUs.jsx
+++ src/views/Home/AboutUs/AboutUs.jsx
@@ -23,8 +23,12 @@ const AboutUs = () => {
       id="about-us"
       sx={{
         gap: "40px",
-        height: "100vh",
+        minHeight: "100vh",
+        width: "80%",
         display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        margin: "auto",
         flexDirection: "column",
         padding: {
           xl: "100px 5rem 20px 5rem",
@@ -33,7 +37,7 @@ const AboutUs = () => {
         },
       }}
     >
-      <InViewComponent>
+      <InViewComponent delay="0.1s">
         <Typography variant="h2">
           {languageState.texts.AboutUs.Title}
         </Typography>
@@ -46,36 +50,180 @@ const AboutUs = () => {
           flexDirection: { xs: "column", md: "row" },
         }}
       >
-        {Object.values(languageState.texts.AboutUs.Main).map((item, i) => (
-          <InViewComponent delay={`0.${i + 4}s`} key={i}>
-            <Box
+        <Box
+          sx={{
+            gap: "100px",
+            display: "flex",
+            flex: "1 1 50%",
+            alignItems: "center",
+            img: {
+              filter: modeState.mode === "light" ? "none" : "invert(1)",
+            },
+          }}
+        >
+          <InViewComponent delay="0.1s">
+            <SitoImage
+              src={aboutUs[languageState.texts.AboutUs.Mission.Image]}
+              alt={languageState.texts.AboutUs.Mission.Title}
               sx={{
-                gap: "30px",
-                display: "flex",
-                flex: "1 1 40%",
-                alignItems: "center",
+                width: "200px",
+                height: "200px",
+                transform: "rotate(20deg)",
+              }}
+            />
+          </InViewComponent>
+          <Box sx={{ display: "flex", flexDirection: "column" }}>
+            <InViewComponent delay="0.5s">
+              <Typography variant="h3">
+                {languageState.texts.AboutUs.Mission.Title}
+              </Typography>
+            </InViewComponent>
+            <InViewComponent delay="0.7s">
+              <Typography sx={{ marginLeft: "5px" }} variant="body1">
+                {languageState.texts.AboutUs.Mission.Description}
+              </Typography>
+            </InViewComponent>
+          </Box>
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          gap: "30px",
+          display: "flex",
+          marginTop: "20px",
+          flexDirection: { xs: "column", md: "row" },
+        }}
+      >
+        <Box
+          sx={{
+            gap: "100px",
+            display: "flex",
+            flex: "1 1 50%",
+            alignItems: "center",
+            img: {
+              filter: modeState.mode === "light" ? "none" : "invert(1)",
+            },
+          }}
+        >
+          <Box sx={{ display: "flex", flexDirection: "column" }}>
+            <InViewComponent delay="0.3s">
+              <Typography variant="h3">
+                {languageState.texts.AboutUs.Vision.Title}
+              </Typography>
+            </InViewComponent>
+            <InViewComponent delay="0.5s">
+              <Typography sx={{ marginLeft: "5px" }} variant="body1">
+                {languageState.texts.AboutUs.Vision.Description}
+              </Typography>
+            </InViewComponent>
+          </Box>
+          <InViewComponent delay="0.7s">
+            <SitoImage
+              src={aboutUs[languageState.texts.AboutUs.Vision.Image]}
+              alt={languageState.texts.AboutUs.Vision.Title}
+              sx={{
+                width: "200px",
+                height: "200px",
                 img: {
-                  filter: modeState.mode === "light" ? "none" : "invert(1)",
+                  transform: "rotateY(-180deg)",
                 },
               }}
-            >
-              <SitoImage
-                src={aboutUs[item.Image]}
-                alt={item.Title}
-                sx={{
-                  width: "105px",
-                  height: "105px",
-                }}
-              />
-              <Box sx={{ display: "flex", flexDirection: "column" }}>
-                <Typography variant="h3">{item.Title}</Typography>
-                <Typography sx={{ marginLeft: "5px" }} variant="body1">
-                  {item.Description}
-                </Typography>
-              </Box>
-            </Box>
+            />
           </InViewComponent>
-        ))}
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          gap: "30px",
+          display: "flex",
+          marginTop: "20px",
+          flexDirection: { xs: "column", md: "row" },
+        }}
+      >
+        <Box
+          sx={{
+            gap: "100px",
+            display: "flex",
+            flex: "1 1 50%",
+            alignItems: "center",
+            img: {
+              filter: modeState.mode === "light" ? "none" : "invert(1)",
+            },
+          }}
+        >
+          <InViewComponent delay="0.1s">
+            <SitoImage
+              src={aboutUs[languageState.texts.AboutUs.Extra.Strategy.Image]}
+              alt={languageState.texts.AboutUs.Extra.Strategy.Title}
+              sx={{
+                width: "200px",
+                height: "200px",
+                img: {
+                  transform: "rotateY(-180deg)",
+                },
+              }}
+            />
+          </InViewComponent>
+          <Box sx={{ display: "flex", flexDirection: "column" }}>
+            <InViewComponent delay="0.3s">
+              <Typography variant="h3">
+                {languageState.texts.AboutUs.Extra.Strategy.Title}
+              </Typography>
+            </InViewComponent>
+            {languageState.texts.AboutUs.Extra.Strategy.Targets.map(
+              (item, i) => (
+                <InViewComponent delay={`0.${1 + 3 * (i + 1)}s`} key={item}>
+                  <Typography sx={{ marginTop: "10px" }} variant="body1">
+                    {item}
+                  </Typography>
+                </InViewComponent>
+              )
+            )}
+          </Box>
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          gap: "30px",
+          display: "flex",
+          marginTop: "20px",
+          flexDirection: { xs: "column", md: "row" },
+        }}
+      >
+        <InViewComponent delay="1.4s">
+          <Box
+            sx={{
+              gap: "100px",
+              display: "flex",
+              flex: "1 1 50%",
+              alignItems: "center",
+              img: {
+                filter: modeState.mode === "light" ? "none" : "invert(1)",
+              },
+            }}
+          >
+            <Box sx={{ display: "flex", flexDirection: "column" }}>
+              <Typography variant="h3">
+                {languageState.texts.AboutUs.Extra.Objectives.Title}
+              </Typography>
+              {languageState.texts.AboutUs.Extra.Objectives.Targets.map(
+                (item) => (
+                  <Typography sx={{ marginTop: "10px" }} variant="body1">
+                    {item}
+                  </Typography>
+                )
+              )}
+            </Box>
+            <SitoImage
+              src={aboutUs[languageState.texts.AboutUs.Extra.Objectives.Image]}
+              alt={languageState.texts.AboutUs.Extra.Strategy.Title}
+              sx={{
+                width: "200px",
+                height: "200px",
+              }}
+            />
+          </Box>
+        </InViewComponent>
       </Box>
     </Box>
   );
